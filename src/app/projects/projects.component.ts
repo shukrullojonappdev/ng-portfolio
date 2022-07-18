55523f1c27dd6c5bfcdd49bcfd955d3a4ea4e06f
shukrullojonappdev <shukrullojon.dev@gmail.com>
2022-07-18 03:19:45 -0700

    portfolio

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -3,21 +3,18 @@ import { faEye } from '@fortawesome/free-solid-svg-icons';
 
 const projects = [
   {
-    name: 'Simple react navigation',
-    img: '',
-    content: 'In this projects I used react, material ui',
-    link: 'https://shsdc.netlify.app',
+    name: 'Online learning landing page',
+    img: '../../assets/shollp.png',
+    link: 'https://shollp.netlify.app',
   },
   {
-    name: 'Nuxtjs portfolio project',
-    img: '',
-    content: 'Nustjs routing, carousel, navigation',
-    link: 'https://shmportfolio.netlify.app',
+    name: 'Food POS Dark',
+    img: '../../assets/shfpd.png',
+    link: 'https://shfpd.netlify.app',
   },
   {
     name: 'Angular weather app',
     img: '',
-    content: 'This application shows weather selected place',
     link: 'https://shngweather.netlify.app',
   },
 ];
